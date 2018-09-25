6684baecd688a6a498e6d4375735a669bee49f82
Nicolas D'Onofrio <nicodonofrio@gmail.com>
2018-09-25 20:42:46 -0300

    Basic assets

---
+++ Assets/TestMove.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Rigidbody>().velocity = new Vector3(-5,0,5);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
