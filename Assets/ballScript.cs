38a6c27f8756a2191cd1a37d250f3a9b3a385d81
matuteale <mbuscagl@itba.edu.ar>
2018-10-08 20:56:12 -0300

    Add pool table

---
+++ Assets/ballScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballScript : MonoBehaviour {
+	private float t = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		t += Time.deltaTime;
+		if (t < 1.0f)
+		{
+			GetComponent<Rigidbody>().velocity = new Vector3(30, 0, 0);
+		}
+	}
+}
