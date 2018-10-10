58b699b0ba3acc995851c5312e617c6799fb763c
matuteale <mbuscagl@itba.edu.ar>
2018-10-10 11:18:42 -0300

    Cap velocity

--- Assets/ballScript.cs
+++ Assets/ballScript.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ballScript : MonoBehaviour {
-	private float t = 0.0f;
+	private Rigidbody rigid;
+	private Vector3 lastVelocity;
 
 	// Use this for initialization
 	void Start () {
-		GetComponent<Rigidbody>().AddForce(Vector3.forward * 1000);
+		rigid = GetComponent<Rigidbody>();
 	}
 
 	// Update is called once per frame
 	void Update () {
+		print(rigid.velocity.magnitude);
+		lastVelocity = rigid.velocity;
+		if(rigid.velocity.magnitude <= 0.8f){
+			rigid.velocity = new Vector3(0,0,0);
+		}else if(rigid.velocity.magnitude >= 20f && lastVelocity.magnitude > 0.8f){
+			rigid.velocity = lastVelocity;
+		}
 	}
 }
