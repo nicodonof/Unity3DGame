8592f6a6eba38d8b0eee8e09365466a7dfc50473
Ezequiel Lynch <ezeqlynch2@gmail.com>
2018-10-10 11:57:01 -0300

    fixed missing brace and moved and renamed ballscript

---
+++ Assets/Scripts/BallScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class BallScript : MonoBehaviour {
+	private Rigidbody rigid;
+	private Vector3 lastVelocity;
+
+	// Use this for initialization
+	private TurnManager tm;
+	
+	void Start () {
+		rigid = GetComponent<Rigidbody>();
+		tm = GameObject.Find("TurnManager").GetComponent<TurnManager>();
+	}
+
+	// Update is called once per frame
+
+	void Update () {
+		print(rigid.velocity.magnitude);
+		lastVelocity = rigid.velocity;
+		if(rigid.velocity.magnitude <= 0.8f){
+			rigid.velocity = new Vector3(0,0,0);
+			if (CompareTag("WhiteBall")) {
+				//mostrar palo
+				//cambiar de turno
+				tm.ChangeTurn();
+			}
+		} else if (rigid.velocity.magnitude >= 20f && lastVelocity.magnitude > 0.8f) {
+			rigid.velocity = lastVelocity;
+			//dentro de cunado frena la bola
+			//en realidad deberia ser cuando todas las bolas frenan
+		
+		}
+	}
+}
