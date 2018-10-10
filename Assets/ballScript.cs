55b3dc5e0447efce455ebf9552c07cdb041d9e59
Ezequiel Lynch <ezeqlynch2@gmail.com>
2018-10-10 11:52:55 -0300

    forgot to save

--- Assets/ballScript.cs
+++ Assets/ballScript.cs
@@ -8,31 +8,25 @@ public class ballScript : MonoBehaviour {
 	private TurnManager tm;
 	
 	void Start () {
-<<<<<<< HEAD
 		rigid = GetComponent<Rigidbody>();
-=======
 		tm = GameObject.Find("TurnManager").GetComponent<TurnManager>();
->>>>>>> created scripts folder, deleted fucking trees
 	}
 
 	// Update is called once per frame
 
 	void Update () {
-<<<<<<< HEAD
 		print(rigid.velocity.magnitude);
 		lastVelocity = rigid.velocity;
 		if(rigid.velocity.magnitude <= 0.8f){
 			rigid.velocity = new Vector3(0,0,0);
 		}else if(rigid.velocity.magnitude >= 20f && lastVelocity.magnitude > 0.8f){
 			rigid.velocity = lastVelocity;
-=======
 		//dentro de cunado frena la bola
 		//en realidad deberia ser cuando todas las bolas frenan
 		if (CompareTag("WhiteBall")) {
 			//mostrar palo
 			//cambiar de turno
 			tm.ChangeTurn();
->>>>>>> created scripts folder, deleted fucking trees
 		}
 	}
 }
