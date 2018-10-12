4cbef180350c6c13a3b118137c4d8c1c608586fb
matuteale <mbuscagl@itba.edu.ar>
2018-10-12 16:58:57 -0300

    Improve pool physics

--- Assets/Scripts/BallScript.cs
+++ Assets/Scripts/BallScript.cs
@@ -9,24 +9,25 @@ public class BallScript : MonoBehaviour {
 
 	// Initial balls positions X, Z in ball number order
 	public static ArrayList InitBallPositions = new ArrayList{
-		new Vector3(12, 14.16f, 0), //WhiteBall
-		new Vector3(-11.7f, 14.16f,-1), //Ball 1
-		new Vector3(-13.5f, 14.16f, 1), //Ball 2
-		new Vector3(-12.6f, 14.16f, -0.51f), //Ball 3
-		new Vector3(-13.5f, 14.16f, -1), //Ball 4
-		new Vector3(-13.5f, 14.16f, -2), //Ball 5
-		new Vector3(-12.6f, 14.16f, 1.53f), //Ball 6
-		new Vector3(-10.9f, 14.16f, 0.51f), //Ball 7
-		new Vector3(-11.7f, 14.16f, 0), //Ball 8
-		new Vector3(-10, 14.16f, 0), //Ball 9
-		new Vector3(-12.6f, 14.16f, 0.51f), //Ball 10
-		new Vector3(-13.5f, 14.16f, 2), //Ball 11
-		new Vector3(-10.9f, 14.16f, -0.51f), //Ball 12
-		new Vector3(-13.5f, 14.16f, 0), //Ball 13
-		new Vector3(-12.6f, 14.16f, -1.53f), //Ball 14
-		new Vector3(-11.7f, 14.16f, 1), //Ball 15
+		new Vector3(12, 14.15235f, 0), //WhiteBall
+		new Vector3(-11.7f, 14.15235f,-1), //Ball 1
+		new Vector3(-13.5f, 14.15235f, 1), //Ball 2
+		new Vector3(-12.6f, 14.15235f, -0.51f), //Ball 3
+		new Vector3(-13.5f, 14.15235f, -1), //Ball 4
+		new Vector3(-13.5f, 14.15235f, -2), //Ball 5
+		new Vector3(-12.6f, 14.15235f, 1.53f), //Ball 6
+		new Vector3(-10.9f, 14.15235f, 0.51f), //Ball 7
+		new Vector3(-11.7f, 14.15235f, 0), //Ball 8
+		new Vector3(-10, 14.15235f, 0), //Ball 9
+		new Vector3(-12.6f, 14.15235f, 0.51f), //Ball 10
+		new Vector3(-13.5f, 14.15235f, 2), //Ball 11
+		new Vector3(-10.9f, 14.15235f, -0.51f), //Ball 12
+		new Vector3(-13.5f, 14.15235f, 0), //Ball 13
+		new Vector3(-12.6f, 14.15235f, -1.53f), //Ball 14
+		new Vector3(-11.7f, 14.15235f, 1), //Ball 15
 		};
 	private float minHeight = 10;
+	private float maxHeight = 14.15235f;
 
 	// Use this for initialization
 	private TurnManager tm;
@@ -42,9 +43,12 @@ public class BallScript : MonoBehaviour {
 	// Update is called once per frame
 
 	void Update () {
+
 		if(transform.position.y <= minHeight){
 			transform.position = (Vector3)InitBallPositions[BallNumber];
 			rigid.velocity = new Vector3(0,0,0);
+		}else if(transform.position.y > maxHeight){
+			transform.position = new Vector3(transform.position.x, maxHeight, transform.position.z);
 		}
 
 		lastVelocity = rigid.velocity;
--- Assets/Scripts/TestMove.cs
+++ Assets/Scripts/TestMove.cs
@@ -39,7 +39,7 @@ public class TestMove : MonoBehaviour {
 			mouseDown = false;
 		}else if(Input.GetMouseButtonUp(0) && mouseDown){
 			Vector3 dir = new Vector3(whiteBall.transform.position.x - transform.position.x, 0f, whiteBall.transform.position.z - transform.position.z);
-			whiteBall.GetComponent<Rigidbody>().AddForce(dir*mouseDownTimer*60);
+			whiteBall.GetComponent<Rigidbody>().AddForce(dir*mouseDownTimer*100);
 			BallScript ballScript = whiteBall.GetComponent<BallScript>();
 			whiteBall.transform.position = new Vector3(whiteBall.transform.position.x, ((Vector3)BallScript.InitBallPositions[ballScript.BallNumber]).y, whiteBall.transform.position.z);
 			tm.Changed = false;
