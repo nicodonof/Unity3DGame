f90783645c7ffb06a35660309af1531d5de864e9
matuteale <mbuscagl@itba.edu.ar>
2018-10-14 18:17:38 -0300

    Add pause menu

---
+++ Assets/Scripts/PauseScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseScript : MonoBehaviour {
+
+	private GameObject PausePanel;
+
+	// Use this for initialization
+	void Start () {
+		PausePanel = GameObject.Find("PausePanel");
+		PausePanel.SetActive(false);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown (KeyCode.Escape)) {
+			if (Time.timeScale == 1) {
+				PauseGame();
+			}else{
+				ResumeGame();
+			}
+		}
+	}
+	private void PauseGame(){
+		Time.timeScale = 0;
+		PausePanel.SetActive(true);
+	}
+	public void ResumeGame(){
+		Time.timeScale = 1;
+		PausePanel.SetActive(false);
+	}
+
+	public void MainMenu(){
+		Time.timeScale = 1;
+		PausePanel.SetActive(false);
+	}
+}
