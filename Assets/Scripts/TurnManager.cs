93ea387f4a551a2f9da3017f6e8a41f689870104
Ezequiel Lynch <ezeqlynch2@gmail.com>
2018-10-17 20:54:02 -0300

    'ui' and start turn bug fix

--- Assets/Scripts/TurnManager.cs
+++ Assets/Scripts/TurnManager.cs
@@ -1,4 +1,5 @@
 ﻿using Boo.Lang;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -13,14 +14,12 @@ public class TurnManager : MonoBehaviour {
 
 	public bool Fault;
 
-	public bool MoveFault;
 	private bool otherFault;
 	private bool stopped;
 
-	private Text turn;
-	private Text playerOne;
-	private Text playerTwo;
-	private Text fault;
+	private TextMeshProUGUI turn;
+	private TextMeshProUGUI playerOne;
+	private TextMeshProUGUI playerTwo;
 	public bool Changed;
 	private TestMove tb;
 	private List<string> currentTurnBallsIn;
@@ -29,16 +28,18 @@ public class TurnManager : MonoBehaviour {
 	public WhiteBall wb;
 	// Use this for initialization
 	void Start () {
+		CurrentTurn = 1;
 		stripedIn = 0;
 		plainIn = 0;
 		PlayerOne = 0;
 		PlayerTwo = 0;
-		Changed = false;
+		Changed = true;
+		Fault = false;
+		otherFault = false;
 		currentTurnBallsIn = new List<string>();
-		turn = GameObject.Find("Turn").GetComponent<Text>();
-		playerOne = GameObject.Find("PlayerOne").GetComponent<Text>();
-		playerTwo = GameObject.Find("PlayerTwo").GetComponent<Text>();
-		fault = GameObject.Find("Fault").GetComponent<Text>();
+		turn = GameObject.Find("Turn").GetComponent<TextMeshProUGUI>();
+		playerOne = GameObject.Find("PlayerOne").GetComponent<TextMeshProUGUI>();
+		playerTwo = GameObject.Find("PlayerTwo").GetComponent<TextMeshProUGUI>();
 		tb = GameObject.Find("taco bilha").GetComponent<TestMove>();
 //		wb = GameObject.Find("whiteBall").GetComponent<WhiteBall>();
 	}
@@ -46,10 +47,9 @@ public class TurnManager : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		turn.text = "player " + (CurrentTurn == 1? "One" : "Two");
-		playerOne.text = "Player1: " + (PlayerOne == 1 ? "Plain" : (PlayerOne == 2 ? "Striped": "Black"));
-		playerTwo.text = "Player2: " + (PlayerTwo == 1 ? "Plain" : (PlayerTwo == 2 ? "Striped": "Black"));
-		fault.text = "Fault: " + Fault;
+		turn.text = "Turn: Player " + CurrentTurn;
+		playerOne.text = "1: " + intToTag(PlayerOne);
+		playerTwo.text = "2: " + intToTag(PlayerTwo);
 	}
 
 	public void ChangeTurn() {
@@ -62,12 +62,11 @@ public class TurnManager : MonoBehaviour {
 			}
 			wb.First = 0;
 
-			if (Fault || MoveFault) { //si hizo falta en este tiro le doy al otro 2 tiros
+			if (Fault) { //si hizo falta en este tiro le doy al otro 2 tiros
 				CurrentTurn = CurrentTurn == 1 ? 2 : 1;
 				currentTurnBallsIn.Clear();
 				otherFault = true;
 				Fault = false;
-				MoveFault = false;
 			} else if (otherFault) { //pasa el primer tiro del q tiene 2 tiros
 				otherFault = false;
 			} else {
@@ -88,10 +87,11 @@ public class TurnManager : MonoBehaviour {
 		} else if (ballTag.Equals("BlackBall")) {
 			if (CurrentTurn == 1 && PlayerOne == 3) {
 				playerOne.text = "Gano Player 1";
+				GetComponent<PauseScript>().ShowGameEndedPanel(playerOne.text);
 			} else if (CurrentTurn == 2 && PlayerTwo == 3) {
 				playerTwo.text = "Gano Player 2";
-			}
-			else {
+				GetComponent<PauseScript>().ShowGameEndedPanel(playerTwo.text);
+			} else {
 				turn.text = "Gano Player " + (CurrentTurn == 1 ? 2 : 1);
 				GetComponent<PauseScript>().ShowGameEndedPanel(turn.text);
 			}
@@ -131,6 +131,16 @@ public class TurnManager : MonoBehaviour {
 			case "StripeBall": return 2;
 			case "PlainBall": return 1;
 			default: return 0;
+		}
+	}
+
+	private string intToTag(int n) {
+		switch (n) {
+			case 0: return "None";
+			case 1: return "Plain";
+			case 2: return "Striped";
+			case 3: return "Black";
+			default: return "";
 		}
 	}
 }
